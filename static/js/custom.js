2865508809c566861d424d993f6a2d21542599d2
Administrator <270624522@qq.com>
2019-06-03 09:30:16 +0800

    修改webksocket

--- static/js/custom.js
+++ static/js/custom.js
@@ -319,7 +319,7 @@ function WebSocketLog(uid, obj_uid) {
     if ("WebSocket" in window) {
         // 打开一个 web socket
         // var host = window.location.host;
-        var ws = new WebSocket("ws://" + location.hostname + ':' + 8000 + "/logs/");
+        var ws = new WebSocket("ws://" + location.hostname + ':' + 8001 + "/logs/");
         ws.onopen = function () {
             // Web Socket 已连接上，使用 send() 方法发送数据
             let message = {'uid': uid, 'obj_uid': obj_uid};
